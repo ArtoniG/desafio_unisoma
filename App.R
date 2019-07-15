af3970383e47d85fd360638f3c8931035aead898
ArtoniG <gui_artoni@hotmail.com>
2019-07-15 15:58:31 -0300

    Criado o arquivo App.R

---
+++ App.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(readxl)
+CCP <- read_xlsx("~/Downloads/Modelo de Dados_CCP_v1.xlsx",col_names = T)
+
+ui <- pageWithSidebar(headerPanel("CCP Project"), 
+                      sidebarPanel(
+                        selectInput(inputId = "variable", label = "Variável:", choices = c())
+                      ), 
+                      mainPanel())
+
+server <-  function(input, output){
+  
+}
+
+shinyApp(ui, server)
