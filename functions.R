c04511c853b7fa66c05549cdf606205c62a6eb7e
ArtoniG <gui_artoni@hotmail.com>
2019-08-04 01:06:23 -0300

    Adicionado esboço da função que retorna os horários de atendimento registrados

--- functions.R
+++ functions.R
@@ -6,6 +6,8 @@ library(Rcpp)
 library(timevis)
 library(lubridate)
 library(tidyverse)
+library(stringr)
+library(hms)
 
 # IMPORTAÇÃO DOS DADOS
 read_excel_allsheets <- function(filename, tibble = T) {
@@ -52,7 +54,7 @@ by()
 funcionarios <- names(mysheets)
 funcionarios <- grep(pattern = "Atendimento", x = funcionarios, value = T, invert = T)
 funcionarios <- grep(pattern = "da Criança", x = funcionarios, value = T, invert = T)
-funcionarios <- funcionarios[-1] # REMOVE A PLANILHA AUXILIAR (PERGUNTAR SE ESTA EXISTE APENAS PARA ESSE BD MODELO)
+funcionarios <- grep(pattern = "Auxiliar", x = funcionarios, value = T, invert = T)
 
 # ENCONTRA AS TABELAS DA ESPECIALIDADE DESEJADA
 find.speciality <- function(especialidade){
@@ -60,10 +62,26 @@ find.speciality <- function(especialidade){
     for (i in 1:length(funcionarios)){ 
    ifelse (colnames(mysheets[[funcionarios[i]]])[1] == especialidade, x[[i]] <- mysheets[[funcionarios[i]]], x[[i]] <- NA)}
   x <- x[!is.na(x)]
-  x
+  ifelse(length(x) == 0, return(c("Não há profissionais atuando na especialidade:", especialidade)), return(x))
 }
 
-check.speciality <- function(find.speciality) ifelse(length(find.speciality) == 0, print("Não há"), NA)
+
+# SUBSTITUI OS VALORES DOS HORÁRIOS NAS PLANILHAS
+#sapply(funcionarios, function(professional.speech){
+# mysheets[[professional.speech]][[1]] <- hms(minutes = minute(mysheets[[professional.speech]][[1]]), hours = hour(mysheets[[professional.speech]][[1]]))
+#})
+
+
+
+
+
+
+
+
+
+
+
+
 
 id.crianca <- unique(mysheets[["Cadastro da Criança"]][,1])
 for (i in 1:length(funcionarios)) {
@@ -107,12 +125,42 @@ check.no.registered <- function(){
                    
                    # SE FALSO: COMPARA APENAS A PARTE DA PLANILHA 
                    false = if(table(mysheets[[comb[1]]][,1:6] == mysheets[[comb[2]]][,1:6])["TRUE"] != 18){
-                      toString(c("Existe(m) criança(s) agendada(s) no mesmo horário com os profissionais:", comb[1], comb[2]))}))
+                      return(print(c("Existe(m) criança(s) agendada(s) no mesmo horário com os profissionais:", comb[1], comb[2])))}))
   }, simplify = F)
 
   # VERIFICA SE HÁ 2 CRIANÇAS AGENDADAS NO MESMO HORÁRIO COM PROFISSIONAIS DIFERENTES  (CHECAR PQ 11 SAÍDAS E NÃO 21)
   verify <- combn(funcionarios, 2, function(comb){
-    if(table(mysheets[[comb[1]]][,1:6] == mysheets[[comb[2]]][,1:6])["TRUE"] != 18){
-              toString(c("Existe(m) criança(s) agendada(s) no mesmo horário com os profissionais:", comb[1], comb[2]))}} , simplify = F)
+    if((table(mysheets[[comb[1]]][,1:6] == mysheets[[comb[2]]][,1:6])["TRUE"]) != 19){
+              return(print(c("Existe(m) criança(s) agendada(s) no mesmo horário com os profissionais:", comb[1], comb[2])))}} , simplify = F)
  
-  
+
+# RETORNA PARA CADA FUNCIONÁRIO OS DIAS DA SEMANA EM QUE HÁ ATENDIMENTO CADASTRADO DE FORMA CATEGORIZADA SEG-1, TER-2, QUA-3, QUI-4 e SEX-5
+dias.da.semana <- function(funcionarios){
+  sapply(funcionarios, function(funci){
+    data <- c(funci)
+    week <- c()
+    ifelse (which(!is.na(mysheets[[data]][,2:6])) >= 1 & which(!is.na(mysheets[[data]][,2:6])) <= 19,  week <- c(week, 1),                      # SEGUNDA
+            ifelse(which(!is.na(mysheets[[data]][,2:6])) >= 20 & which(!is.na(mysheets[[data]][,2:6])) <= 38, week <- c(week, 2),               # TERÇA
+                   ifelse(which(!is.na(mysheets[[data]][,2:6])) >= 39 & which(!is.na(mysheets[[data]][,2:6])) <= 57, week <- c(week, 3),        # QUARTA
+                          ifelse(which(!is.na(mysheets[[data]][,2:6])) >= 58 & which(!is.na(mysheets[[data]][,2:6])) <= 76, week <- c(week, 4), # QUINTA
+                                 week <- c(week, 5)))))                                                                                         # SEXTA
+  }, USE.NAMES = T)  
+}
+
+horarios.atend <- function(funcionarios){
+  semana <- dias.da.semana(funcionarios)
+  horarios <- list()
+  for(i in seq_along(semana)){
+    if(length(semana[[i]]) > 0){
+      dias <- unique(semana[[i]])
+      sapply(dias, function(j){
+        horarios[[funcionarios[i]]] <<- which(!is.na(mysheets[[funcionarios[i]]][[dias[j]+1]]))
+      })
+    }
+  }
+  return(horarios)
+}
+
+
+              
+
